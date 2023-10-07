5cdfe9e61f2543fc241d518741f0f358deb2c2be
Kirill Semenov <semenov.develop@gmail.com>
2023-10-07 21:03:38 +0500

    feat: 🎸 Update last udpate logic

--- src/components/last-update.tsx
+++ src/components/last-update.tsx
@@ -14,16 +14,17 @@ export const LastUpdate = ({ updatedAt, className }: LastUpdateProps) => {
   const [updatedAtSeconds, setUpdatedAtSeconds] = useState(msToS(updatedAt));
 
   useEffect(() => {
-    const interval = setInterval(() => setUpdatedAtSeconds(msToS(updatedAt)), 1000);
+    setUpdatedAtSeconds(msToS(updatedAt));
+    const interval = setInterval(() => setUpdatedAtSeconds(msToS(updatedAt)), 10000);
 
     return () => clearInterval(interval);
   }, [updatedAt]);
 
   return (
-    <div className={clsx('p-3 flex items-center gap-1 text-xs bg-accent justify-center', className)}>
+    <div className={clsx('p-2 flex items-center gap-1 text-xs bg-accent justify-center', className)}>
       <FontAwesomeIcon icon={faArrowRotateRight} size="xs" />
       <span className="text-primary">Last update:</span>
-      <span className="text-secondary">{`${updatedAtSeconds} seconds ago`}</span>
+      <span className="text-secondary">{updatedAtSeconds < 10 ? 'Just now' : `${updatedAtSeconds} seconds ago`}</span>
     </div>
   );
 };
