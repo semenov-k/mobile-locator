e8a6171580cd0d5a77d822c96af94297e23c9096
Kirill Semenov <semenov.develop@gmail.com>
2023-10-07 21:01:40 +0500

    style: 💄 Update font weight

--- src/components/position-section.tsx
+++ src/components/position-section.tsx
@@ -7,11 +7,11 @@ export const PositionSection = ({ latitude, longitude }: PositionSectionProps) =
   return (
     <section className="flex flex-col gap-2 p-6 text-accent text-s">
       <div className="flex justify-between">
-        <span>LATITUDE</span>
+        <span className="font-light">LATITUDE</span>
         <span>{latitude ?? '--'}</span>
       </div>
       <div className="flex justify-between">
-        <span>LONGITUDE</span>
+        <span className="font-light">LONGITUDE</span>
         <span>{longitude ?? '--'}</span>
       </div>
     </section>
