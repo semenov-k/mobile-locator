f5d80317a9e75a060874753e04d21d86a8bf30ba
Kirill Semenov <semenov.develop@gmail.com>
2023-10-07 21:02:32 +0500

    feat: 🎸 Add throttling for poisition events

--- src/hooks/use-geolocation.ts
+++ src/hooks/use-geolocation.ts
@@ -1,5 +1,6 @@
 import { useEffect, useMemo, useState } from 'react';
 import { pick, equals } from 'ramda';
+import { throttle } from 'throttle-debounce';
 
 type Position = {
   latitude: number | null;
@@ -9,6 +10,8 @@ type Position = {
   speed: number | null;
 };
 
+const UPDATES_THROTTLE_TIMEOUT = 500;
+
 export const useGeolocation = () => {
   const [error, setError] = useState<string | null>(null);
   const [position, setPosition] = useState<Position>({
@@ -16,23 +19,29 @@ export const useGeolocation = () => {
     longitude: null,
     altitude: null,
     altitudeAccuracy: null,
-    speed: null, // TODO: m/s -> km/h
+    speed: null,
   });
   const [updatedAt, setUpdatedAt] = useState<number>();
 
   const isSupporting = !!navigator.geolocation;
 
+  const positionCallback: PositionCallback = useMemo(
+    () =>
+      throttle(UPDATES_THROTTLE_TIMEOUT, ({ timestamp, coords }: GeolocationPosition) => {
+        const newPosition = pick(['altitude', 'latitude', 'longitude', 'speed', 'altitudeAccuracy'], coords);
+        if (!equals(newPosition, position)) {
+          setPosition(newPosition);
+        }
+        setUpdatedAt(timestamp);
+        setError(null);
+      }),
+    [position],
+  );
+
   useEffect(() => {
     if (isSupporting) {
       const watchId = navigator.geolocation.watchPosition(
-        ({ timestamp, coords }) => {
-          const newPosition = pick(['altitude', 'latitude', 'longitude', 'speed', 'altitudeAccuracy'], coords);
-          if (!equals(newPosition, position)) {
-            setPosition(newPosition);
-          }
-          setUpdatedAt(timestamp);
-          setError(null);
-        },
+        positionCallback,
         (error) => {
           if (error.code === error.PERMISSION_DENIED) {
             setError('Access to geolocation denied');
@@ -46,8 +55,10 @@ export const useGeolocation = () => {
       );
 
       return () => navigator.geolocation.clearWatch(watchId);
+    } else {
+      setError('Geolocation is not supporting');
     }
-  }, [isSupporting, position]);
+  }, [isSupporting, positionCallback]);
 
   return useMemo(
     () => ({
